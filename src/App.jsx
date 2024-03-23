90963c90f7d4db29bffc4f9c406ce14c731b0682
AbhishekKr-Jha <akjha4127@gmail.com>
2024-03-23 18:50:33 +0530

    ..

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from "react"
+import Navbar from "./Navbar"
+function App() {
+
+  return (
+   <>
+   
+   <div className="w-screen h-screen bg-red-900 px-3  max-w-[2000px]">
+<Navbar/>
+
+  </div>
+   </>
+  )
+}
+
+export default App
